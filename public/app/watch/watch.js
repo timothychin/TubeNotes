c4502d6e5c23a08f1bc4432f54eaceca75f22fca
adamjung <ajung91@hotmail.com>
2016-12-08 11:46:34 -0800

    Add html skeleton for user's note into watch.html

--- public/app/watch/watch.js
+++ public/app/watch/watch.js
@@ -4,4 +4,9 @@ angular.module('tubenotes.watch', [])
   $scope.getUrl = function(){
     return $sce.trustAsResourceUrl("https://www.youtube.com/embed/4ZAEBxGipoA");
   }
+
+  $scope.postNote = function(title, note) {
+    console.log('title is: ', title);
+    console.log('note is: ', note);
+  }
 });
