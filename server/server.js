86fa766a0463a0aaadf7a032b1bd9af49c9026a2
Anton <anton.arboleda2gmail.com>
2016-12-07 21:18:42 -0800

    (feat) Add post and get routes for all comments and videos

--- server/server.js
+++ server/server.js
@@ -6,72 +6,44 @@ var bodyParser = require('body-parser');
 
 var db = require('./schemas');
 
-// api - require username
+// API - require username
 app.get('/videos', function (req, res) {
-  //find userID from user
-    // get all videos where video forieng key equals user id
-    // get all comments
-    // then send them back to the client
-    
-  res.send()
+  var results = [];
+  db.User.findOne({where: {username: req.body.username}}).then(function (user) {
+    db.Video.findAll({where: {userId: user.get('id')}}).then(function (videos) {
+      for (var i = 0; i < videos.length; i++) {
+        var video = videos[i];
+        db.Comment.findAll({where: {videoId: video.get('id')}}).then(function (comments) {
+          var videoObject = {
+            url: video.url,
+            title: video.title,
+            comments: comments
+          };
+          results.push(videoObject);
+          if (i === videos.length) {
+            res.status(200).send(results);
+          }
+        });
+      }
+    });
+  });
 });
 
-app.get('/test', function (req, res) {
-  db.User.findOrCreate({where: {username: 'ANTON'}})
-    .then(function (user) {
-      // user[0].dataValues.id
-      console.log(user[0].get('id'), 'userId');
-      db.Video.findOrCreate({where: { url: 'youtube.com', title: 'CatVideo', UserId: user[0].get('id') }})
-        .then(function (video) {
-          db.Comment.findOrCreate({ where: { title: 'This is a comment title', text: 'comment text', UserId: user[0].get('id'), VideoId: video[0].get('id') }})
-        })
-  })
 
-    res.send('sent');
-});
-
-app.get('/test-video', function () {
-  db.Video.findOrCreate({where: {url: 'https://www.youtube.com/embed/4ZAEBxGipo', title: 'Superman', userId: 1}}).spread(function (user, created) {
-    console.log(user.get({
-      plain: true
-    }))
-    console.log(created);
-  }) 
-})
-
-
-// from client 
-  // username
-  // videourl and video title
-  // comment title, comment text, comment timestamp
-  // find or create video given req.body.url and req.body.videoTitle
-  // find or create comment given req.body.commentTitle, commentText, and commentTimestamp
-  // then create associations
-
-  // check it the request parameters are valid
-  //write to db 
 app.post('/comment-video', function (req, res) {
-  //find or create username given req.body
   db.User.findOrCreate({where: {username: req.body.username}})
     .then(function (user) {
       db.Video.findOrCreate({where: { url: req.body.url, title: req.body.title, userId: user[0].get('id') }})
         .then(function (video) {
-          db.Comment.findOrCreate({ where: { title: req.body.title, text: req.body.text, userId: user[0].get('id'), videoId: video[0].get('id') }})
-        })
-    })
-
-  res.status(200).send('sent')
+          db.Comment.findOrCreate({ where: { title: req.body.title, text: req.body.text, userId: user[0].get('id'), videoId: video[0].get('id') }});
+        });
+    });
+  res.status(201).send('sent');
 });
 
-
-
-
-
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
-
+  console.log('Example app listening on port 3000!');
+});
 
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
