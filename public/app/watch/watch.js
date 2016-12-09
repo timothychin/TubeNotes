824c18bb64a1be20bb405d19b2ca6dfe4b91448d
adamjung <ajung91@hotmail.com>
2016-12-08 18:47:16 -0800

    Refactor to fix iframe API loading issues

--- public/app/watch/watch.js
+++ public/app/watch/watch.js
@@ -1,16 +1,54 @@
 angular.module('tubenotes.watch', [])
 
-.controller('WatchController', function($scope, $sce) {
-  $scope.currentVideoTime = '';
-  $scope.currentVideoUrl = $sce.trustAsResourceUrl('https://www.youtube.com/embed/oTSGrL04wwU');
+.controller('WatchController', function($scope, $sce, $interval, AppFactory) {
+  var interval;
+  $scope.currentVideoTime = 0;
 
-  $scope.postNote = function(title, note) {
-    console.log('title is: ', title);
-    console.log('note is: ', note);
-    $scope.changeUrl('https://www.youtube.com/embed/dQw4w9WgXcQ');
+  // var player;
+  window.onYouTubeIframeAPIReady = function() {
+    window.player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'M7lc1UVf-VE',
+      events: {
+        // 'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
   }
 
-  $scope.changeUrl = function(url) {
-    $scope.currentVideoUrl = $sce.trustAsResourceUrl(url);
+  window.onPlayerStateChange = function(event) {
+    // if video is playing, update 
+    if (event.data === YT.PlayerState.PLAYING) {
+      // set video to current time
+      console.log('player.getCurrentTime() is', player.getCurrentTime());
+      $scope.currentVideoTime = Math.floor(player.getCurrentTime());
+      console.log('currentVideoTime is', $scope.currentVideoTime);
+      // set interval for time setting
+      interval = $interval(() => ($scope.currentVideoTime = Math.floor(player.getCurrentTime()))
+      , 100);
+    } else if (event.data === YT.PlayerState.ENDED || 
+       event.data === YT.PlayerState.PAUSED) {
+
+    }
+  }
+
+  $scope.postNote = function(title, note) {
+    // $scope.changeUrl('https://www.youtube.com/embed/dQw4w9WgXcQ');
+    console.log(Math.floor(window.player.getCurrentTime()));
+  }
+
+  $scope.getUrl = function() {
+    var currentVideoUrl = "htttps://www.youtube.com/embed/" + AppFactory.currentVideo.id;
+    return $sce.trustAsResourceUrl(currentVideoUrl);
+    // $scope.currentVideoUrl = $sce.trustAsResourceUrl(url);
+  }
+
+  $scope.getVideoTime = function() {
+    if (window.player){
+      return window.player.getCurrentTime();
+    } else {
+      return 0;
+    }
   }
 });
