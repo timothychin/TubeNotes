ef661d1f7ec5dd747a1b6481cac892104e45eb44
Nicholas Wang <nickwang55@gmail.com>
2016-12-08 19:59:31 -0800

    Add library video routes

--- public/app/search/search.js
+++ public/app/search/search.js
@@ -7,11 +7,22 @@ angular.module('tubenotes.search', [])
 
   console.log('search controller is loaded');
 
+  // This is to set the current video from the YouTube search
   $scope.setCurrentVideo = function (video) {
     AppFactory.currentVideo = {
       title: video.snippet.title,
       id: video.id.videoId
-    }
+    };
+    $location.path('/watch');
+  };
+
+  // This is to set the current video to a video that you have already annotated
+  $scope.setCurrentLibraryVideo = function (video) {
+    AppFactory.currentLibraryVideo = {
+      title: video.title,
+      url: video.url,
+      comments: video.comments
+    };
     $location.path('/watch');
   };
 
@@ -24,6 +35,8 @@ angular.module('tubenotes.search', [])
   }).then(function(response) {
     // Store the results of the get request in $scope.userVideos
     $scope.userVideos = response.data;
+    AppFactory.videoLibrary = response.data;
+    console.log('videoLIbrary: ', AppFactory.videoLibrary);
   });
 
   $scope.searchYoutube = function(msg) {
