f7e368c47c9dc01ee72a7fb925559decbdb38a25
adamjung <ajung91@hotmail.com>
2016-12-11 14:12:49 -0800

    Implement correct note time functionality -Show the time the user started typing into the input field instead of time when the user clicks on the 'post' button

--- public/app/watch/watch.js
+++ public/app/watch/watch.js
@@ -1,8 +1,11 @@
 angular.module('tubenotes.watch', [])
 
 .controller('WatchController', function($scope, $sce, $interval, AppFactory) {
+  var startTime = 0;
   var intervalPromise;
   $scope.currentVideoTime = '00:00';
+  $scope.noteTimestamp = '';
+  // scope variable for user note input
   $scope.videoComments = AppFactory.currentVideo.comments;
 
   window.onYouTubeIframeAPIReady = function() {
@@ -46,23 +49,50 @@ angular.module('tubenotes.watch', [])
     return `${minutes}:${seconds}`
   }
 
+  $scope.setTimestamp = function() {
+    // only set time stamp on user's first input
+    if ($scope.userNote.note.$pristine) {
+      startTime = Math.floor(player.getCurrentTime());   
+      $scope.noteTimestamp = $scope.formatTime(startTime);   
+    }
+  }
+
+  $scope.resetNote = function() {
+    // empty input fields
+    $scope.title = '';
+    $scope.note = '';
+
+    // reset $pristine flags on form
+    $scope.userNote.title.$setPristine();
+    $scope.userNote.note.$setPristine();
+
+    // reset startTime and $scope.noteTimestamp to initial values
+    startTime = 0;
+    $scope.noteTimestamp = '';
+  }
+
   $scope.postNote = function(title, note) {
-    var timestamp = Math.floor(player.getCurrentTime());
+    // var timestamp = Math.floor(player.getCurrentTime());
+    console.log('comment timestamp is', startTime);
     // add note to current video's comments array
     AppFactory.currentVideo.comments.push(
       { title: title,
         text: note,
-        timestamp: timestamp });
+        timestamp: startTime });
 
     // update scope variable to make comments render on page
     $scope.videoComments = AppFactory.currentVideo.comments;
 
     // call update to server for the current video
     AppFactory.addNote(title, note, timestamp);
+    var startTime = 0;
   }
 
-  $scope.clickNote = function() {
+  $scope.clickNote = function(comment) {
     // when note is clicked
     // start playing video at note's timestamp
+    if (window.player) {
+      window.player.seekTo(comment.timestamp, false);
+    }
   }
 });
