e0aa37673a8c18db6f87382329cee97cc9c8024d
Anton <anton.arboleda2gmail.com>
2016-12-10 16:48:38 -0800

    (bug) Unable to fix module dependency issue

--- public/app/auth/auth.js
+++ public/app/auth/auth.js
@@ -1,21 +1,27 @@
-angular.module('tubenotes.search', [])
+angular.module('tubenotes.auth', [])
 
 .controller('AuthController', function ($scope, $window, $location, Auth) {
   $scope.user = {};
+
   $scope.login = function () {
-    Auth.signin($scope.user)
+    Auth.login($scope.user)
       .then(function (token) {
-        $window.localStorage.setItem('com.tubenotes',token);
-        $location.path('/search')
+        $window.localStorage.setItem('com.tubenotes', token);
+        $location.path('/search');
       })
       .catch(function (error) {
         console.error(error);
-      })
-  }
+      });
+  };
+
   $scope.signup = function () {
-    //write Auth.signup()
-      //then
-        //set item to local storage
-      //catch errors
-  }
-})
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.tubenotes', token);
+        $location.path('/search');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+});
---
+++ public/app/services/services.js
@@ -0,0 +1,41 @@
+angular.module('tubenotes.services', [])
+
+.factory('Auth', function ($http, $location, $window) {
+  var login = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/login',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var signup = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var isAuth = function () {
+    return !!$window.localStorage.getItem('com.tubenotes');
+  };
+
+  var signout = function () {
+    $window.localStorage.removeItem('com.tubenotes');
+    $location.path('/login');
+  };
+
+  return {
+    login: login,
+    signup: signup,
+    isAuth: isAuth,
+    signout: signout
+  };
+});
